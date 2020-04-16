e8b579828413ed59d94ced95558c337f6704f267
Gary Dix <intelproof@gmail.com>
2020-04-16 13:23:44 +1200

    holiday match

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -9,10 +9,31 @@ export function loadAssessmentsFile(startDate) {
   console.log("date.js dateStart in", startDate);
   var date = new Date(startDate); //convert string to date
 
+  var allHolidays = GenerateHolidayDates();
+
   //loop through each entry and calculate dates adding days to assessment
+  //moment(date, "DD-MM-YY") this is only parsing date, not formatting date
   const AssWithDates = data.map(item => {
-    var newdate = new Date(moment(date, "DD-MM-YY").add(item.days, "d")); //date.setDate(date.getDate() + item.days));
-    item.DueDate = moment(newdate).format("dddd, MMMM Do"); //format date for viewing
+    var newdate = new Date(moment(date, "DD-MM-YY").add(item.days, "d"));
+    newdate = moment(newdate).format("DD-MM-YY");
+
+    //compare holiday dates with assessment dates
+    allHolidays.map(item => {
+      console.log("date match assess date ", newdate);
+      // console.log("date match holiday date ", item.daysbreak);
+      //if the day is in the holiday dates
+      if (item.daysbreak.includes(newdate)) {
+        // make the duedate the last day of the holiday and count the date difference to add to the folling dates
+        newdate = new Date(
+          moment(item.daysbreak[item.daysbreak.length - 1]).format("DD-MM-YY")
+        );
+        console.log("date match ", "true " + newdate);
+        //    newdate.add(item.days,"d"); //add in the holidays
+      }
+      console.log("date match ", "false");
+    });
+
+    item.DueDate = moment(newdate).format("MMM Do"); //format date for viewing
     console.log("out", item.DueDate);
     return item;
   });
@@ -31,7 +52,7 @@ export function GenerateHolidayDates() {
 
   //push all the days that are a break to daysbreak array
   const allHolidays = holidays.map(item => {
-    var holidayStartDate = new Date(moment(item.startDate, "DD-MM-YYYY")); //turn the date string to a date
+    var holidayStartDate = new Date(moment(item.startDate, "DD-MM-YY")); //turn the date string to a date
     //loop through the days and add 1
     var i = 1;
     for (i = 0; i < item.days; i++) {
@@ -40,12 +61,15 @@ export function GenerateHolidayDates() {
       );
       //holidayStartDate.setDate(holidayStartDate.getDate() + i)
       //);
-      item.daysbreak.push(singledate.toDateString()); //add dates to the array
 
-      console.log(
-        item.name + " day count " + item.daysbreak.length,
-        item.daysbreak.toString()
-      );
+      var dateFinal = moment(singledate).format("DD-MM-YY");
+
+      item.daysbreak.push(dateFinal); //add dates to the array
+
+      // console.log(
+      //   item.name + " day count " + item.daysbreak.length,
+      //   item.daysbreak.toString()
+      // );
     }
     return item;
   });
